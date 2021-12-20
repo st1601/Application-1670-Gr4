aab851213b7dc34c54d3b837f7e851c927fa944f
st1601 <87642035+st1601@users.noreply.github.com>
2021-12-20 15:35:20 +0700

    ok

---
+++ models/admin.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongodb');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Admin = new Schema({
+    username: {type: String, maxLength: 255},
+    name: {type: String, maxLength: 255},
+    dateofbirth: {type: Date, maxLength: 255},
+});
+
+module.exports = mongoose.model('Admin', Admin);
